1fab8b6ccd9ddd393ad94dd8f0fd9ff5ef6337aa
Ryan <ryanpauley@gmail.com>
2015-01-10 21:30:20 +1100

    websockets controller in place.

---
+++ app/helpers/chat_contoller_helper.rb
@@ -0,0 +1,2 @@
+module ChatContollerHelper
+end
---
+++ test/controllers/chat_contoller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ChatContollerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
